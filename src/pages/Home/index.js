c83ee35bbe75f6f256fa225e098f965b6f608c57
domenicha <domenica.chavez.g@gmail.com>
2023-02-06 18:40:29 -0500

    Leccion 17 completa presenta errores

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Title, Subtitle } from './styles';
 
 function Home() {
-    return <h1>Home</h1>;
+    return (
+    <>
+      <Title>Home</Title>
+      <Subtitle>Esta es mi casa</Subtitle>
+      <a href="/cart">Cart</a>
+      <ul>
+        <li>List 1</li>
+        <li>List 2</li>
+      </ul>
+    </>
+);
 }
 
 export default Home;
